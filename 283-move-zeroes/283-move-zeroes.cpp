ad975dc5202b13d54ddd0ddab30712f636f4fe8d
klakshya17 <56078582+klakshya17@users.noreply.github.com>
2022-06-12 16:13:40 +0530

    Time: 49 ms (20.85%), Space: 19.1 MB (83.56%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int left=0,zero=0;
-        // while(left<nums.size() && zero<nums.size()){
-        //     while(zero<nums.size() && nums[zero]!=0)
+        
+        //not optimal
+        // while(left<nums.size()){
+        //     // zero=0;
+        //     while(zero < nums.size() && nums[zero]!=0)
         //         zero++;
-        //     while(left<nums.size() && nums[left]==0)
-        //         left++;
-        //     if(left<zero)
+        //     if(zero>=nums.size()-1)
         //         break;
-        //     if(left<nums.size() && zero<nums.size() )
-        //         swap(nums[left],nums[zero]);
+        //     left=zero;
+        //     while(left<nums.size() && nums[left]==0 )
+        //         left++;
+        //     if(left>=nums.size())
+        //         break;
+        //     swap(nums[zero],nums[left]);
         // }
-        while(left<nums.size()){
-            // zero=0;
-            while(zero < nums.size() && nums[zero]!=0)
-                zero++;
-            if(zero>=nums.size()-1)
-                break;
-            left=zero;
-            while(left<nums.size() && nums[left]==0 )
-                left++;
-            if(left>=nums.size())
-                break;
-            swap(nums[zero],nums[left]);
+        
+        int curr=0,prev=0;
+        for(;curr<nums.size();curr++){
+            if(nums[curr]!=0)
+                swap(nums[prev++],nums[curr]);
         }
     }
 };
