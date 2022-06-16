c3d339bda4e056a0ae6717f01868517f8547d3cb
klakshya17 <56078582+klakshya17@users.noreply.github.com>
2022-06-17 02:28:14 +0530

    Time: 47 ms (34.77%), Space: 23.3 MB (12.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,31 +10,42 @@
 
 class Solution {
 public:
-    TreeNode* lca(TreeNode* root,TreeNode* a, TreeNode* b){
-      if(root==NULL)
-          return NULL;
+//     TreeNode* lca(TreeNode* root,TreeNode* a, TreeNode* b){
+//       if(root==NULL)
+//           return NULL;
       
-      // if(root->left == a || root->left == b)
-      //     return root->left;
-      // if(root->right == a || root->right == b)
-      //     return root->right;
+//       // if(root->left == a || root->left == b)
+//       //     return root->left;
+//       // if(root->right == a || root->right == b)
+//       //     return root->right;
         
-        if(root==a || root==b)
-            return root;
+//         if(root==a || root==b)
+//             return root;
      
-      TreeNode * p = lca(root->left,a,b);
-      TreeNode * q = lca(root->right,a,b);
+//       TreeNode * p = lca(root->left,a,b);
+//       TreeNode * q = lca(root->right,a,b);
       
         
-        if(p==NULL && q==NULL)
+//         if(p==NULL && q==NULL)
+//             return NULL;
+//         else if(p && q)
+//             return root;
+//         else if(p)
+//             return p;
+//         else return q;
+//     }
+    TreeNode* lca(TreeNode *root,TreeNode* a, TreeNode* b){
+        if(root == NULL)
             return NULL;
-        else if(p && q)
+        if(root->val >= a->val && root->val <= b ->val)
             return root;
-        else if(p)
-            return p;
-        else return q;
+        if(root->val >= a->val && root->val >= b ->val)
+            return lca(root->left,a,b);
+        else return lca(root->right,a,b);
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val<q->val)
         return lca(root,p,q);
+        else return lca(root,q,p);
     }
 };
