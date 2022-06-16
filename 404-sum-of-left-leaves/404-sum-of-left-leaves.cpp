a949ab42942598c453a5a11a34839f919d7f32d5
klakshya17 <56078582+klakshya17@users.noreply.github.com>
2022-06-17 01:23:33 +0530

    Time: 13 ms (6.82%), Space: 13.6 MB (10.86%) - LeetHub

---
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    void func(TreeNode* root, bool isleft){
+        if(root==NULL){
+            return;
+        }
+        
+        if(root->left==NULL && root->right==NULL){
+            if(isleft){
+                count+= root->val;
+            }
+            return;
+        }
+        
+        func(root->left, 1);
+        func(root->right, 0);
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        func(root, 0);
+        return count;
+    }
+};
